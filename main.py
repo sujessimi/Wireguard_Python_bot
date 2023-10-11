437854737064cdfc79e66e5b1ba6ffe0378fe87c
sujessimi <59653436+sujessimi@users.noreply.github.com>
2023-10-11 12:41:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ YOUR_SERVER_PORT = "51830"
 def get_next_available_ip() -> str:
     with open('/etc/wireguard/wg0.conf', 'r') as f:
         contents = f.read()
-    
     all_ips = re.findall(r"10.0.0.(\d+)", contents)
     if not all_ips:
         return "10.0.0.2"
@@ -20,8 +19,7 @@ def get_next_available_ip() -> str:
     return f"10.0.0.{last_ip + 1}"
 
 def start(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.from_user.id
-
+    user_id = update.effective_user.id
     if user_id == ADMIN_ID:
         keyboard = [
             [
@@ -37,16 +35,14 @@ def start(update: Update, context: CallbackContext) -> None:
 def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
-
     if query.data == 'new_user':
         private_key = subprocess.getoutput('wg genkey')
         public_key = subprocess.getoutput(f'echo {private_key} | wg pubkey')
         user_ip = get_next_available_ip()
-
         config = f"""[Interface]
 PrivateKey = {private_key}
 Address = {user_ip}/24
-DNS = 8.8.8.8  # или любой другой DNS
+DNS = 8.8.8.8
 
 [Peer]
 PublicKey = {SERVER_PUBLIC_KEY}
@@ -55,7 +51,6 @@ Endpoint = {YOUR_SERVER_IP}:{YOUR_SERVER_PORT}
 """
         with open('/etc/wireguard/wg0.conf', 'a') as f:
             f.write(config)
-
         query.edit_message_text(text=f"Конфигурация создана:\n{config}")
     elif query.data == 'reboot_server':
         subprocess.run(['sudo', 'reboot'])
@@ -66,10 +61,8 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CallbackQueryHandler(button))
-
     updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
     main()
-
